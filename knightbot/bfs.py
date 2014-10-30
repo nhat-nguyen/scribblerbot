456d0c70ccd0d6da6fde56cd56120a51ad8ad2fa
Hoang Nhat <hoangnhat2911@gmail.com>
2014-10-30 00:55:18 -0400

    breadth-first search now fully fuctional

--- knightbot/bfs.py
+++ knightbot/bfs.py
@@ -1,53 +1,57 @@
 import numpy
+from random import *
 
 graph = numpy.zeros((8, 8))
 
-x = 0
-y = 0
+start = (randint(0, 7), randint(0, 7))
+stop = (randint(0, 7), randint(0, 7))
 
+print "Start: ", start
+print "Stop: ", stop
 
-dst = (1, 1)
-# def knightMove(pos, dst, graph, frontier):
+print "------------------------------\n"
 
-# 	movement = [((frontier[0])[0] - 1, (frontier[0])[1] - 2), ((frontier[0])[0] + 1, (frontier[0])[1] - 2), ((frontier[0])[0] - 1, (frontier[0])[1] + 2), ((frontier[0])[0] + 1, (frontier[0])[1] + 2), ((frontier[0])[0] - 2, (frontier[0])[1] - 1), ((frontier[0])[0] - 2, (frontier[0])[1] + 1), ((frontier[0])[0] + 2, (frontier[0])[1] - 1), ((frontier[0])[0] + 2, (frontier[0])[1] + 1)]
+frontier = [start]
+came_from = {}
 
-# 	del frontier[0]
-
-# 	for i in movement:
-# 		if not graph[i[0]][i[1]]:
-# 			graph[i[0]][i[1]] = 1
-# 			if (i[0], i[1]) == dst:
-# 				break
-# 			frontier.append((i[0], i[1]))
-
-
-frontier = [(x, y)]
-
-stop = False
-
+stopFind = False
 while len(frontier) > 0:
-	# knightMove(frontier[0], (1, 1), graph, frontier)
+	# directions that the knight can move
 	movement = [((frontier[0])[0] - 1, (frontier[0])[1] - 2), ((frontier[0])[0] + 1, (frontier[0])[1] - 2), ((frontier[0])[0] - 1, (frontier[0])[1] + 2), ((frontier[0])[0] + 1, (frontier[0])[1] + 2), ((frontier[0])[0] - 2, (frontier[0])[1] - 1), ((frontier[0])[0] - 2, (frontier[0])[1] + 1), ((frontier[0])[0] + 2, (frontier[0])[1] - 1), ((frontier[0])[0] + 2, (frontier[0])[1] + 1)]
-	print '----------'
+
+	for i in movement:
+		# check only the valid movement
+		if i[0] >= 0 and i[0] < 8 and i[1] >= 0 and i[1] < 8:
+
+			if not graph[i[0]][i[1]]:
+				graph[i[0]][i[1]] = i[0] + i[1] / 10.0
+				if (i[0], i[1]) == stop:
+					graph[i[0]][i[1]] = "99"
+					stopFind = True
+				frontier.append(i)
+				if i not in came_from:
+					came_from[i] = frontier[0]
+				if stopFind:
+					break
+	if stopFind:
+		break
 
 	del frontier[0]
 
-	print movement
-	for i in movement:
-		print graph
-		print '------'
-		if ([i])[0] > 0 and ([i])[0] < 8 and ([i])[1] > 0 and ([i])[1] < 8:
-			if not graph[i[0]][i[1]]:
-				graph[i[0]][i[1]] = 1
-				if (i[0], i[1]) == dst:
-					graph[i[0]][i[1]] = 9
-					stop = True
-					break
-				frontier.append((i[0], i[1]))
-	if stop:
-		break
-
-
-
-
 print graph
+
+current = stop
+steps = [current]
+while current != start:
+	current = came_from[current]
+	steps.append(current)
+
+
+print "\n------------------------------\n"
+
+
+i = len(steps) - 1
+
+while i >= 0:
+	print steps[i]
+	i -= 1
